d5d3bae868a9fde312643ac53884d26621cf2206
Myself-Alien <pritimaypanda1993@gmail.com>
2024-09-15 20:34:30 +0530

    Second Changes
    Second Changes

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Shopy || Online Shopping</title>
+    <link href="../_dist/css/styles.css" rel="stylesheet">
+    <link href="../_dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../_dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container=fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Shopy</h2>
+            </div>
+            <div class="col-md-8"></div>
+        </div>
+    </div>
+</body>
--- public/index.php
+++ public/index.php
@@ -2,11 +2,13 @@
 <html>
 
 <head>
-    <title>Shopy</title>
+    <title>Shopy || Online Shopping</title>
     <link href="../_dist/css/styles.css" rel="stylesheet">
     <link href="../_dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="../_dist/js/bootstrap.min.js"></script>
 </head>
 <body>
-    
+    <div class="container=fluid">
+        <div class="row"></div>
+    </div>
 </body>
