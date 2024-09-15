f4a152d924082152937d6d9af895f2c1d85ce8d5
Myself-Alien <pritimaypanda1993@gmail.com>
2024-09-16 00:06:01 +0530

    Added slider
    Added slider & fix image size and caption

--- includes/header.php
+++ includes/header.php
@@ -7,15 +7,36 @@
     <link href="../_dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="../_dist/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-    <div class="container=fluid">
-        <div class="row">
-            <div class="col-md-4">
-                <h2>Shopy</h2>
-            </div>
-            <div class="col-md-8">
-                
+    <div class="container-fluid header_top">
+        <div class="container">
+            <div class="d-flex">
+                <div class="p-2 flex-grow-1">Call - 1800 254 1144</div>
+                <div class="p-2">Privacy</div>
+                <div class="p-2">Media</div>
+                <div class="p-2">Media</div>
+                <div class="p-2">Contact</div>
             </div>
         </div>
     </div>
+
+    </div>
+    <div class="container-fluid menu_area">
+        <div class="container">
+            <div class="row pt-3 pb-3">
+                <div class="col-md-4">
+                    <h2 class="logo mb-0"><a href="index.php">Shopy</a></h2>
+                </div>
+                <div class="col-md-8">
+                    <ul class="menu_area_menu mb-0 float-end">
+                        <li><a href="login.php">Login/Signup</a></li>
+                        <li><a href="">My Account</a></li>
+                        <li><a href="">Cart</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php
+include('../includes/header.php');
+?>
 <!DOCTYPE html>
 <html>
 
@@ -7,8 +10,50 @@
     <link href="../_dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="../_dist/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-    <div class="container=fluid">
-        <div class="row"></div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 no-pad">
+                <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../_dist/images/slider1.jpg" class="d-block w-100 " alt="...">
+                            <div class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center bottom-0">
+                                <h1 class="">First slide label</h1>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../_dist/images/slider2.jpg" class="d-block w-100 " alt="...">
+                            <div class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center bottom-0">
+                            <h1>First slide label</h1>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
+    <script>
+        const myCarouselElement = document.querySelector('#carouselExampleCaptions')
+
+        const carousel = new bootstrap.Carousel(myCarouselElement, {
+            interval: 4000,
+            touch: false
+        })
+    </script>
 </body>
