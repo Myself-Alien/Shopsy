b6617c305f3a58753feb0bf929729af9c8aabcea
Myself-Alien <pritimaypanda1993@gmail.com>
2024-09-16 01:34:21 +0530

    Create Signup Form

--- public/signup.php
+++ public/signup.php
@@ -1,22 +1,48 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Shopy || Login</title>
     <link href="../_dist/css/styles.css" rel="stylesheet">
     <link href="../_dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="../_dist/js/bootstrap.min.js"></script>
 </head>
+
 <body>
     <div class="container-fluid wrapp">
-        <div class="container mt-5 pt-5">
-            <div class="col-md-4 offset-md-4 admin_box">
+        <div class="container pt-5">
+            <div class="col-md-6 offset-md-3 admin_box">
                 <h2 class="text-center">Login</h2>
                 <form action="" method="post">
-                    <div class="input-group mb-3">
-                        <input type="text" name="email" class="form-control no_radius" placeholder="Enter Email" required>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="text" name="fname" class="form-control no_radius" placeholder="First Name" required>
+                        </div>
+                        <div class="col">
+                            <input type="text" name="lname" class="form-control no_radius" placeholder="Last Name" required>
+                        </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="pass" class="form-control no_radius" placeholder="Enter Password" required>
+                        <input type="email" name="email" class="form-control no_radius" placeholder="Email*">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" name="pass" class="form-control no_radius" placeholder="Password*">
+                    </div>
+                    Address
+                    <div class="input-group mb-3">
+                        <input type="text" name="street " class="form-control no_radius" placeholder="Street Address">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" name="city " class="form-control no_radius" placeholder="City">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" name="state " class="form-control no_radius" placeholder="State">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="number" name="zip " class="form-control no_radius" placeholder="Zip">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" name="country " class="form-control no_radius" placeholder="Country">
                     </div>
                     <input type="submit" name="submit" class="btn btn-primary no_radius my_btn_dark" value="Submit">
                 </form>
@@ -24,4 +50,5 @@
         </div>
     </div>
 </body>
+
 </html>
