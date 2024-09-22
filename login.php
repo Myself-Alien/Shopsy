a8122b48e39b2796caeefb5845191c6d6967aa2c
Myself-Alien <pritimaypanda1993@gmail.com>
2024-09-22 23:15:25 +0530

    Some Minor Changes

--- login.php
+++ login.php
@@ -20,18 +20,18 @@ include('header.php');
                         <h2 class="text-center black">Login</h2>
                         <form action="login_action.php" method="post">
                             <div class="input-group mb-3">
-                                <input type="text" name="email" class="form-control no_radius" placeholder="Enter Email" required>
+                                <input type="text" name="email" id="email" class="form-control no_radius" placeholder="Enter Email" required>
                             </div>
                             <div class="input-group mb-3">
-                                <input type="password" name="pass" class="form-control no_radius" placeholder="Enter Password" required>
+                                <input type="password" name="pass" is="pass" class="form-control no_radius" placeholder="Enter Password" required>
                             </div>
-                            <input type="submit" name="submit" class="btn btn-primary no_radius my_btn_dark" value="Submit">
+                            <input type="submit" name="submit" value="Submit" class="btn btn-primary no_radius my_btn_dark" value="Submit">
                         </form>
                     </div>
                     <div class="col-md-6 admin_box_extra">
                        <div class="h-100 align-items-center justify-content-center admin_box_side">
                         <div class="row">
-                            <div class="col-12 pb-1">A New Account?</div>
+                            <div class="col-12 pb-1 nacc_text">A New User?</div>
                             <div class="col-12">
                             <button type="button" class="my_btn_color signup_btn"><a href="signup.php">Sign Up</a></button>
                             </div> 
@@ -43,6 +43,10 @@ include('header.php');
             </div>
         </div>
     </div>
-</body>
+    <script>
+        const emailEl = document.querySelector('#email');
+        const passwordEl = document.querySelector('#pass');
 
+    </script>
+</body>
 </html>
